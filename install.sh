04cc2eb708888dd154ae0450e94e282d2041ebce
Tetsorou <103302601+Tetsorou@users.noreply.github.com>
2025-03-06 14:01:17 -0600

    a

--- install.sh
+++ install.sh
@@ -9,7 +9,7 @@ fi
 # Check if using NixOS. If not using NixOS, script will exit.
 if [[ ! "$(grep -i nixos < /etc/os-release)" ]]; then
 	echo "This installation script only works on NixOS! Download an iso at https://nixos.org/download/"
-  echo "Keep in mind that this script is not intended for use while in the live environment."
+  echo "Keep in mind that this script is not intended for use while in the live environment." 
 	exit 1
 fi
 
